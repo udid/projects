a4bff02934f22b925a5ed9c194c6477689b664ec
g1L <danblab@gmail.com>
2013-07-19 05:29:46 +0300

    Added hash_table.h, and implemented part of what fault_callback should do. hash_table.h implements our hash table for pages, so that we can know if a page is already in the list at ~ O(1) time. Adding and removing items from hash table is O(1) exactly, while finding an element costs ~ O(1). Still need to complete fault_callback, and by then the algorithm is mostly ready. Tested compilation, still need to run & debug!

---
+++ phase_shift_algorithm_module/hash_table.h
@@ -0,0 +1,85 @@
+#ifndef __PHASE_SHIFTS_HASH_TABLE
+#define __PHASE_SHIFTS_HASH_TABLE
+
+#include <linux/list.h>
+#include <linux/hash.h>
+#include <linux/slab.h>
+#include <linux/log2.h>
+
+/** Hash table allocation.
+ * @hash_size - must be a power of 2!
+ */
+static inline struct hlist_head* alloc_hash(unsigned long hash_size)
+{
+	unsigned long i;
+	struct hlist_head* hash_tbl = (struct hlist_head*) kmalloc ( hash_size* sizeof(struct hlist_head), GFP_KERNEL);
+	for(i = 0UL; i < hash_size; i++)
+	{
+		INIT_HLIST_HEAD(&hash_tbl[i]);
+	}
+	return hash_tbl;
+}
+
+/** Adding a locality_page* to the hash table.
+ * @hash_tbl - the hast table to be added to.
+ * @hash_size - the hash table's size.
+ * @page - the struct locality_page to be added.
+ * @key - the key that is used in the hash table.
+ */ 
+static inline void hash_add(struct hlist_head* hash_tbl, unsigned long hash_size, struct locality_page* page, unsigned long key)
+{
+	INIT_HLIST_NODE(&page->hash_list);
+	hlist_add_head(&page->hash_list, &hash_tbl[hash_long(key, ilog2(hash_size))]);
+} 
+
+/** Looks for an element of locality_page* with the given key. Returns NULL such one doesn't exist.
+ * @hash_tbl - hash table.
+ * @hash_size - hash table's size.
+ * @key - the key used to search.
+ */ 
+static inline struct locality_page* hash_find(struct hlist_head* hash_tbl, unsigned long hash_size, unsigned long key)
+{
+	struct hlist_head* head = &hash_tbl[hash_long(key, ilog2(hash_size))];
+	struct locality_page* page;
+	struct hlist_node* ptr;
+	hlist_for_each_entry(page, ptr, head, hash_list)
+	{
+		if(page->nm_page == key)
+			return page;
+	}
+	return NULL;
+}
+/** Removes a page from its hash table.
+ * @page - page that is being removed from the hash table.
+ */ 
+static inline void hash_del(struct locality_page* page)
+{
+	hlist_del(&page->hash_list);
+}
+/** Frees the memory used by the hash table, and frees all struct locality_page* structures. 
+ ** It also removes each of the struct locality_page* from list of locality, thus also removing the list.
+ * @hash_tbl - hash table.
+ * @hash_size - the hash table's size.
+ */ 
+static inline void free_hash_list(struct hlist_head* hash_tbl, unsigned long hash_size)
+{
+	unsigned long i;
+	struct hlist_node* tmp;
+	struct hlist_node* ptr;
+	struct locality_page* page;
+	for(i = 0UL; i < hash_size; i++)
+	{
+		hlist_for_each_entry_safe(page, ptr, tmp, &hash_tbl[i], hash_list)
+		{
+			hlist_del(&page->hash_list);
+			list_del(&page->locality_list);
+			kfree(page);
+		}
+	}
+	kfree(hash_tbl);
+}
+
+
+
+
+#endif
--- phase_shift_algorithm_module/phase_algorithm.c
+++ phase_shift_algorithm_module/phase_algorithm.c
@@ -1,23 +1,59 @@
 #include <linux/init.h>
 #include <linux/module.h>
-
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/cdev.h>
 #include <linux/phase_shifts.h>
 #include <linux/cred.h>
 
+#include <asm/page_types.h>
+
 #include "phase_shifts_data.h"
-#define current_cred() \
-     rcu_dereference_protected(current->cred, 1)
+#include "hash_table.h"
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-static uid_t watchedUser = (uid_t) 2013;
+static uid_t WATCHED_USER = (uid_t) 2013;
 
-static void free_phase_shifts_data(struct phase_shift_detection_scheme* scheme)
+static unsigned long HASH_SIZE = 16;
+static unsigned long LOCALITY_SIZE = 64;
+
+/** Function that extracts the page number out of an address.
+ * @address - address from which we would like to extract page number.
+ */
+static inline unsigned long get_pn(unsigned long address)
 {
+	return address >> PAGE_SHIFT;
+}
+
+
+static struct locality_page* alloc_locality_page(unsigned long address)
+{
+	struct locality_page* page = (struct locality_page *) kmalloc(sizeof(struct locality_page), GFP_KERNEL);
+	page->nm_page = get_pn(address);
+	INIT_HLIST_NODE(&page->hash_list);
+	INIT_LIST_HEAD(&page->locality_list);
+	
+	return page;
+}
+static void phase_shifts_data_init (struct phase_shift_detection_scheme* scheme)
+{
+	scheme->locality_hash_tbl = alloc_hash(HASH_SIZE);
+	INIT_LIST_HEAD(&scheme->locality_list);
+	scheme->locality_list_size = 0;
+	scheme->locality_max_size = LOCALITY_SIZE;
+	scheme->hash_table_size = HASH_SIZE;
+	scheme->current_tick_faults = 0;
+	scheme->previous_tick_faults = 0;
+	spin_lock_init(&scheme->lock);
+	
+}
+static void free_phase_shifts_data (struct phase_shift_detection_scheme* scheme)
+{
+	// This function also removes the locality pages themselves, thus removing the locality list.
+	free_hash_list(scheme->locality_hash_tbl, scheme->hash_table_size);
 	kfree(scheme);
+	printk ( KERN_ALERT "phase_shifts_detector: cleaned info. \n" );
 }
 
 static void exit_callback (struct task_struct* p)
@@ -25,7 +61,7 @@ static void exit_callback (struct task_struct* p)
 	char buff[TASK_COMM_LEN];
 	if(p->phase_shifts_private_data)
 	{
-		kfree(p->phase_shifts_private_data);
+		free_phase_shifts_data(p->phase_shifts_private_data);
 		p->phase_shifts_private_data = NULL;
 		printk( KERN_ALERT "%s [%d]: execution has ended. \n", get_task_comm(buff, p), task_pid_nr(p) );
 	}
@@ -34,13 +70,10 @@ static void exec_callback(struct task_struct* p)
 {
 	struct phase_shift_detection_scheme* data;
 	char buff[TASK_COMM_LEN];
-	if(task_uid(p) == watchedUser)
+	if(task_uid(p) == WATCHED_USER)
 	{
 		data = (struct phase_shift_detection_scheme*) kmalloc(sizeof(struct phase_shift_detection_scheme), GFP_KERNEL);
-		
-		data->current_tick_faults = 0;
-		data->previous_tick_faults = 0;
-		spin_lock_init(&data->lock);
+		phase_shifts_data_init(data);
 		if(p->phase_shifts_private_data)
 		{
 			free_phase_shifts_data(p->phase_shifts_private_data);
@@ -58,27 +91,58 @@ static void fault_callback (struct mm_struct *mm,
 		     pte_t *pte, pmd_t *pmd, unsigned int flags)
 {
 	struct phase_shift_detection_scheme* scheme = current->phase_shifts_private_data;
+	struct locality_page* page;
 	pte_t entry;
 	//spinlock_t* ptl;
 	
 	if(scheme)
 	{
 		entry = *pte;
-		if(!pte_present(entry))
+		
+		if(!pte_present(entry) && (vma->vm_flags & VM_EXEC)) // Page not in memory, and the memory region is executable - 1st case.
 		{
-			if(vma->vm_flags & VM_EXEC)
+			spin_lock(&scheme->lock);
+			
+			page = hash_find(scheme->locality_hash_tbl, scheme->hash_table_size, get_pn(address));
+			if(page) // If page already in lists.
 			{
-				spin_lock(&scheme->lock);
-				scheme->current_tick_faults++;
-				spin_unlock(&scheme->lock);
+				// Moves the element to the head (new references would cause it to move back to head of locality list.
+				list_del(&page->locality_list);
+				INIT_LIST_HEAD(&page->locality_list);
+				list_add(&page->locality_list, &scheme->locality_list);
 			}
+			else // Otherwise, page is not already in lists.
+			{
+				if(scheme->locality_list_size == scheme->locality_max_size) // Checks if list is full... :(
+				{
+					/* In this case, needs to handle that last page is removed from list, hash table, by getting last element in list, then using list_del, hash_del.
+					 * Must then make sure to flag user/supervisor bit of the removed page! Still need to figure out how to do it...
+					 */
+				}
+				else // Can add without issues.
+				{
+					// Adds our new page to the list and hash table.
+					page = (struct locality_page*) alloc_locality_page(address);
+					list_add(&page->locality_list, &scheme->locality_list);
+					hash_add(scheme->locality_hash_tbl,  scheme->hash_table_size, page, page->nm_page);
+					scheme->locality_list_size++;
+				}
+			}
+			
+			scheme->current_tick_faults++;
+			spin_unlock(&scheme->lock);
+		
+		}
+		else
+		{
+			
 		}
 	}
 }
 
 
 /**
- * Timer callback - older version which should still be updated. Not tested yet!
+ * Timer callback. Tested and doesn't crash atm.
  */
 static void timer_callback (struct task_struct* p, int user_tick)
 {	
