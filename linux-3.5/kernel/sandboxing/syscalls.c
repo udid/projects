1524db1de4c9398c3fab82d107471de21de12b72
Asaf Cohen <asafc@me.com>
2013-08-04 22:23:47 +0300

    syscall blocking does not work at the moment..

--- linux-3.5/kernel/sandboxing/syscalls.c
+++ linux-3.5/kernel/sandboxing/syscalls.c
@@ -10,19 +10,16 @@
 
 SYSCALL_DEFINE0(get_sandbox_id)
 {
-  printk(KERN_ALERT "called sys_get_sandbox_id(), returning %ld\n", current->sandbox_id);
   return current->sandbox_id;
 }
 
 SYSCALL_DEFINE1(switch_sandbox, unsigned long, sandbox_id)
 {
-  printk(KERN_ALERT "called sys_switch_sandbox(%ld)\n", sandbox_id);
   if (0 != current->sandbox_id) {
     printk(KERN_ALERT "switch_sandbox() is available only to sandbox 0 processes.\n");
     return -EINVAL;
   }
   current->sandbox_id = sandbox_id;
-  printk(KERN_ALERT "sandbox_id is now %ld\n", current->sandbox_id);
   return 0;
 }
 
